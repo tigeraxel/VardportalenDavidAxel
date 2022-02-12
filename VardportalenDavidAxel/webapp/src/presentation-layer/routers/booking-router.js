9b11d3170352540af422d75a10f304576f5f4bc4
David Wennerblom <weda20ih@student.ju.se>
2022-02-12 16:57:35 +0100

    added verification that both checkboxes are not checked when registering new booking

--- VardportalenDavidAxel/webapp/src/presentation-layer/routers/booking-router.js
+++ VardportalenDavidAxel/webapp/src/presentation-layer/routers/booking-router.js
@@ -17,8 +17,18 @@ router.get('/', function (request, response) {
 })
 
 router.post('/newBooking', function(request, response){
+
     request.body.no = Boolean(request.body.no)
     request.body.yes = Boolean(request.body.yes)
+
+    if(request.body.no && !request.body.yes){
+        console.log("Har inte haft corona")
+    }
+    else if(request.body.yes && !request.body.no){
+        console.log("har haft corona!")
+    }else{
+        console.log("error, båda fälten får inte vara ifyllda")
+    }
     console.log(request.body.yes)
     console.log(request.body.no)
     if(request.body.yes && request.body.no){
